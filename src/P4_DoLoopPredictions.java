10a3a32c4fe5dc8f6b2a13af3142c580fd910c94
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-22 20:48:22 -0500

    Update P4_DoLoopPredictions.java

--- src/P4_DoLoopPredictions.java
+++ src/P4_DoLoopPredictions.java
@@ -1,7 +1,7 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/22/2018
 */
 
 /*
@@ -11,27 +11,27 @@
  What do you expect the loop to output?
  
  (a)
- Repeats:
- Expected output:
+ Repeats: 10
+ Expected output: (starts at 1 then +10 every loop till 91)1 11 21 31 41 51 61 71 81 91
  (b)
- Repeats:
- Expected output:
+ Repeats:Inf
+ Expected output: An infinite counting down number
  (c)
- Repeats:
- Expected output:
+ Repeats: Inf
+ Expected output: 250 forever. Conditions to stop loop will never be met
  (d)
- Repeats:
- Expected output:
+ Repeats:5 times
+ Expected output: bbbbbabbbbb 5 b's on boths sides of word "a"
    
  */
 
 /*
  Was your prediction correct?
  
- (a)
- (b)
- (c)
- (d)
+ (a)yes
+ (b)yes
+ (c)yes
+ (d)yes
 
 */
 public class P4_DoLoopPredictions {
@@ -41,8 +41,16 @@ public class P4_DoLoopPredictions {
 		
 		
 		// copy and paste the loop to test between here, between the println statements
+		//d
+		String word = "a";
 		
-				
+		do {
+			
+		    word = "b" + word + "b";
+		    
+		} while( word.length() < 10 );
+		System.out.println( word );
+						
 		System.out.println("ENDING LOOP TEST");
 
 	}
